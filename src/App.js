1e3c7979fed9e14b8d7cec0cb5edb0cfa384a133
wongband <bmwong@ucdavis.edu>
2020-09-18 16:31:21 -0700

    Completed Step2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,86 @@
+import React from 'react';
+import { Button, Card, Col, Row, Space } from 'antd';
+import { getBackgroundType, getPokemonImage, getType } from 'helper/pokemonHelpers';
+import styled from 'styled-components';
+
+const StyledButton = styled(Button)`
+  background-color: ${props => props.typeName};
+  color: white;
+  border: none;
+  width: ${props => props.width}%;
+  margin-right: 5px;
+  margin-top: 5px;
+`;
+
+const StyledContainer = styled.div`
+  margin: auto;
+  width: 65%;
+  padding: 10px;
+`;
+
+const StyledCard = styled(Card)`
+  background-color: ${props => props.typeName};
+  margin: 30px;
+  height: 180px;
+  border-radius: 27px;
+  color: white;
+
+  :hover {
+    cursor: pointer;
+    float: top;
+  }
+
+  .ant-card-body {
+    padding: 20px;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: 80%;
+  height: 100%;
+`;
+
+const StyledTitle = styled.h2`
+  color: white;
+`;
+
 const App = () => {
-  return null;
-}
+  const name = 'Bryan Wong';
+
+  return (
+    <StyledContainer className='site-card-wrapper'>
+      <h1>Hi there, my name is {name}! Welcome to my Pokedex!</h1>
+      <p>Hi Im a paragraph in React</p>
+      <Row>
+        <Col span={8}>
+          <StyledCard typeName={getBackgroundType('grass')}>
+            <Space align='start'>
+              <div>
+                <StyledTitle>Bulbasaur</StyledTitle>
+                <StyledButton
+                  typeName={getType('grass')}
+                  width={'100'}
+                  shape='round'
+                  size='small'
+                >
+                  Grass
+                </StyledButton>
+                <StyledButton
+                  typeName={getType('poison')}
+                  width={'100'}
+                  shape='round'
+                  size='small'
+                >
+                  Poison
+                </StyledButton>
+              </div>
+              <StyledImage alt='' src={getPokemonImage('1')} />
+            </Space>
+          </StyledCard>
+        </Col>
+      </Row>
+    </StyledContainer>
+  );
+};
 
 export default App;
