21f44e061aaebf8c1bd4271ad86a74bfdf42137c
wongband <bmwong@ucdavis.edu>
2020-09-17 18:27:27 -0700

    Finished Card Part of JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { Card } from 'antd';
+import { getPokemonImage } from 'helper/pokemonHelpers';
+
 const App = () => {
-  return null;
-}
+  const name = 'Bryan Wong';
+
+  return (
+    <div>
+      <h1>Hi there, my name is {name}! Welcome to my Pokedex!</h1>
+      <p>Hi Im a paragraph in React</p>
+      <Card>
+        <img alt='' src={getPokemonImage('1')} />
+      </Card>
+    </div>
+  );
+};
 
 export default App;
