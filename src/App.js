f08f0c76e1e0cb3ddd2d69f7d2ca53d27b134c63
wongband <bmwong@ucdavis.edu>
2020-09-24 21:26:35 -0700

    Finished code for step 6

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
-import { Button, Card } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Row } from 'antd';
+import { loadPokemon } from 'helper/pokemonHelpers';
 import styled from 'styled-components';
+import PokeCard from 'components/PokeCard';
+import PokeModal from 'components/PokeModal';
 
-const StyledButton = styled(Button)`
-  background-color: ${props => props.typeName};
-  color: white;
-  border: none;
-  width: ${props => props.width}%;
-  margin-right: 5px;
-  margin-top: 5px;
-`;
-
-const StyledCard = styled(Card)`
-  background-color: ${props => props.typeName};
-  margin: 30px;
-  height: 180px;
-  border-radius: 27px;
-  color: white;
-
-  :hover {
-    cursor: pointer;
-    float: top;
-  }
-
-  .ant-card-body {
-    padding: 20px;
-  }
+const StyledContainer = styled.div`
+  margin: auto;
+  width: 65%;
+  padding: 10px;
 `;
 
 const App = () => {
-  return null;
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const loadedPokemon = async () => {
+      try {
+        const apiResponse = await loadPokemon();
+        setPokemon(apiResponse.results);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    loadedPokemon();
+  }, []);
+
+  return (
+    <StyledContainer className='site-card-wrapper'>
+      {selectedPokemon !== null ? (
+        <PokeModal
+          pokemonDetail={selectedPokemon}
+          changeSelected={setSelectedPokemon}
+        />
+      ) : null}
+      <Row>
+        {pokemon?.map(selectedPokemon => (
+          <PokeCard
+            key={selectedPokemon.name}
+            name={selectedPokemon.name}
+            url={selectedPokemon.url}
+            changeSelected={setSelectedPokemon}
+          />
+        ))}
+      </Row>
+    </StyledContainer>
+  );
 };
 
 export default App;
---
+++ src/components/PokeCard.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Space } from 'antd';
+import {
+  getBackgroundType,
+  getPokemonImage,
+  loadSelectedPokemon
+} from 'helper/pokemonHelpers';
+import { uppercaseWord } from 'helper/shared';
+import styled from 'styled-components';
+import PokeType from 'components/PokeType';
+
+const StyledCard = styled(Card)`
+  background-color: ${props => props.typeName};
+  margin: 30px;
+  height: 180px;
+  border-radius: 27px;
+  color: white;
+  :hover {
+    cursor: pointer;
+    float: top;
+  }
+  .ant-card-body {
+    padding: 20px;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: 80%;
+  height: 100%;
+`;
+
+const StyledTitle = styled.h2`
+  color: white;
+`;
+
+const PokeCard = props => {
+  const [pokemonDetail, setPokemonDetail] = useState(null);
+
+  useEffect(() => {
+    const selectedPokemon = async () => {
+      try {
+        const apiResponse = await loadSelectedPokemon(props.url);
+        setPokemonDetail(apiResponse);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    selectedPokemon();
+  }, [props.url]);
+
+  return (
+    <Col span={8}>
+      <StyledCard
+        typeName={getBackgroundType(pokemonDetail?.types[0].type.name)}
+        onClick={() => props.changeSelected(pokemonDetail)}
+      >
+        <Space align='start'>
+          <div>
+            <StyledTitle>{uppercaseWord(props.name)}</StyledTitle>
+            <PokeType types={pokemonDetail?.types} width='100' />
+          </div>
+          <StyledImage alt='' src={getPokemonImage(pokemonDetail?.id)} />
+        </Space>
+      </StyledCard>
+    </Col>
+  );
+};
+
+export default PokeCard;
