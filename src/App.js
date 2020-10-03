882b21b1cf1dc48124bb277a314bdcedd8f243a9
wongband <bmwong@ucdavis.edu>
2020-10-03 15:07:03 -0700

    Updated step-3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Row } from 'antd';
 import PokeCard from 'components/PokeCard';
-=======
-import { Button, Card } from 'antd';
->>>>>>> 45e66452e4488725359a71bcf96e15964d651ab1
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
